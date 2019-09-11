57b32e4caa1db8539c194101d6d5d62471f28136
Metin Lamby <metin.lamby@outlook.com>
2019-09-11 18:45:25 +0200

    added the seed file

--- app/models/garden.rb
+++ app/models/garden.rb
@@ -2,8 +2,8 @@ class Garden < ApplicationRecord
   belongs_to :user
   has_many :bookings
 
-  validates :title, :description, :photo, :price, presence: true
+  # validates :title, :description, :photo, :price, presence: true
 
-  validates :title, length: { in: 10..30 }
-  validates :size, inclusion: { in: ["Flower Garden", "Container Garden", "Yard", "Back Garden", "Botanical Garden", "Tropical Garden"]}
+  validates :title, length: { in: 100..300 }
+  validates :garden_type, inclusion: { in: ["Flower Garden", "Container Garden", "Yard", "Back Garden", "Botanical Garden", "Tropical Garden"]}
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,56 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+# Destroying db entries
+puts "Destroying all db entries"
+
+Booking.destroy_all
+Garden.destroy_all
+User.destroy_all
+
+puts "Finished destroying"
+
+
+# Ceating fake users
+puts "Creating fake users..."
+
+20.times do |user|
+  user = User.new(
+    name: Faker::Name.first_name,
+    email: Faker::Internet.email,
+    password: "123456")
+  user.save!
+end
+
+puts "Created #{User.count} fake users..."
+
+
+# Creating fake gardens
+puts "Creating fake gardens..."
+
+garden_types_array = ["Flower Garden", "Container Garden", "Yard", "Back Garden", "Botanical Garden", "Tropical Garden"]
+
+20.times do |garden|
+  garden = Garden.new(
+    description: Faker::Lorem.sentence(word_count: 35),
+    title: Faker::Lorem.sentence(word_count: 15),
+    garden_type: garden_types_array.sample,
+    price: rand(5..20).to_s,
+    user_id: rand(User.first.id..User.last.id))
+  garden.save!
+end
+
+puts "Created #{Garden.count} fake gardens..."
+
+
+# Creating fake bookings
+
+puts "Creating fake bookings..."
+
+10.times do |booking|
+  booking = Booking.new(
+    garden_id: rand(Garden.first.id..Garden.last.id),
+    user_id: rand(User.first.id..User.last.id))
+  booking.save
+end
+
+puts "Created #{Booking.count} fake bookings..."
