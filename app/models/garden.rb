c2eb514f8f1c038371ca09ecf4311d2679c3fa77
Metin Lamby <metin.lamby@outlook.com>
2019-09-11 18:50:26 +0200

    commented out the validation

--- app/models/garden.rb
+++ app/models/garden.rb
@@ -4,6 +4,6 @@ class Garden < ApplicationRecord
 
   # validates :title, :description, :photo, :price, presence: true
 
-  validates :title, length: { in: 100..300 }
+  # validates :title, length: { in: 100..300 }
   validates :garden_type, inclusion: { in: ["Flower Garden", "Container Garden", "Yard", "Back Garden", "Botanical Garden", "Tropical Garden"]}
 end
