dafe16e023fe8e8d30eda8b0f5db9d343a00de1c
Metin Lamby <metin.lamby@outlook.com>
2019-09-12 13:10:20 +0200

    adited the front end of the home page

---
+++ app/javascript/components/_navbar_scroll.js
@@ -0,0 +1,11 @@
+const navbar = document.getElementById("nav");
+
+window.onscroll = function(){
+  if (window.pageYOffset > 80) {
+    navbar.style.background = "white";
+    console.log("Hello");
+  }
+  else {
+    navbar.style.background = "transparent";
+  }
+}
